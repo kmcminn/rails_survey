b5bad5e3c0e21ca27da2d77982da60d0a32617e6
kmcminn <tenyou@gmail.com>
2013-08-02 03:47:55 -0700

    adding other vendor bits

---
+++ vendor/gems/nokogiri-1.6.0/ext/nokogiri/tmp/x86_64-linux-gnu/ports/libxslt/1.1.26/libxslt-1.1.26/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/karsten/dev/rails_survey/vendor/gems/nokogiri-1.6.0/ports/x86_64-linux-gnu/libxslt/1.1.26/lib"
+XSLT_LIBS="-lxslt  -L/home/karsten/dev/rails_survey/vendor/gems/nokogiri-1.6.0/ports/x86_64-linux-gnu/libxml2/2.8.0/lib -lxml2 -lz -lm -lm"
+XSLT_INCLUDEDIR="-I/home/karsten/dev/rails_survey/vendor/gems/nokogiri-1.6.0/ports/x86_64-linux-gnu/libxslt/1.1.26/include"
+MODULE_VERSION="xslt-1.1.26"
